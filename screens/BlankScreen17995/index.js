d4a537aaf9750062bebd46d3ee799994205e52c6
Crowdbotics <team@crowdbotics.com>
2023-02-28 23:41:21 +0000

    Add Blank Screen 17995

---
+++ screens/BlankScreen17995/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen17995 = () => {
+  return <View style={_styles.BlankScreen17995}>
+      </View>;
+};
+
+export default BlankScreen17995;
+
+const _styles = StyleSheet.create({
+  BlankScreen17995: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
